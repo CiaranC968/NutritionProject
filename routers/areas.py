45ad62000cd9417384887426b392e74ca8f271f1
Ciaran Cairns <ciarancairns@googlemail.com>
2025-02-20 23:19:10 +0000

    Fixed up text

--- routers/areas.py
+++ routers/areas.py
@@ -2,16 +2,16 @@ from fastapi import APIRouter
 from internal.NutritionApi import MealdbApi
 
 arouter = APIRouter(prefix="/areas",
-                    tags=["areas"])
+                    tags=["Areas"])
 
 
 nutrition = MealdbApi()
 
-@arouter.get("/", summary = "areas")
+@arouter.get("/")
 async def get_areas():
     return nutrition.all_areas()
 
 
-@arouter.get("/{area}", summary="area")
+@arouter.get("/{area}", summary="Get food by area")
 async def get_area(area: str):
     return nutrition.search_by_area(area)
--- routers/meals.py
+++ routers/meals.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from internal.NutritionApi import MealdbApi
 
 mrouter = APIRouter(prefix="/meals",
-                    tags=["meals"])
+                    tags=["Meals"])
 
 nutrition = MealdbApi()
 
@@ -12,7 +12,7 @@ async def get_meal_categories():
     return nutrition.meal_categories()
 
 
-@mrouter.get("/{mealid}", summary="Get category")
+@mrouter.get("/{mealid}", summary="Get Category by ID")
 async def get_meal_categories(mealid: int):
     return nutrition.search_by_meal_id(mealid)
 
